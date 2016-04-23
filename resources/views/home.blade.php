41d4daa31f7f2a5fe0cbf22ede23512465a9683b
Wes Thorburn <wjthorburn@live.com.au>
2016-04-23 23:09:08 +1000

    Adjust authenticated/default navigation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,22 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
+        	@if(\Auth::user())
+        		<div class="panel panel-default">
+	                <div class="panel-heading">Dashboard</div>
+	                <div class="panel-body">
+	                    You are logged in!
+	                </div>
+	            </div>
+	        @else
+	        	<div class="panel panel-default">
+	                <div class="panel-heading">Welcome</div>
+	                <div class="panel-body">
+	                    Your Application's home Page.
+	                </div>
+	            </div>
+	        @endif
+            
         </div>
     </div>
 </div>
