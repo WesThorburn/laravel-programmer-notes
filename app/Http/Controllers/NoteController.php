b1a8c8974cae2af2955a9f004edbe995dc2a45dd
Wes Thorburn <wjthorburn@live.com.au>
2016-04-24 22:40:09 +1000

    Create Note model, built note store function

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Models\Note;
 
 class NoteController extends Controller
 {
@@ -13,5 +14,14 @@ class NoteController extends Controller
     		'problem' => 'required|string',
     		'solution' => 'required|string'
     	]);
+
+    	$note = new Note;
+	    $note->user_id = \Auth::user()->id;
+	    $note->problem = $request->problem;
+	    $note->solution = $request->solution;
+	    $note->save();
+
+	    \Session::flash('noteSaveSuccess', 'Your Note was created successfully!');
+        return redirect('/');
     }
 }
