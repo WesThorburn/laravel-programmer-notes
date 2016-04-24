3ecb5977194b9cdd649a24d25b6edbef329913e1
Wes Thorburn <wjthorburn@live.com.au>
2016-04-24 22:12:28 +1000

    Add migration for notes table

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -9,6 +9,9 @@ use App\Http\Requests;
 class NoteController extends Controller
 {
     public function store(Request $request){
-    	dd($request);
+    	$this->validate($request, [
+    		'problem' => 'required|string',
+    		'solution' => 'required|string'
+    	]);
     }
 }
