4105a7ea9621b7d16dc3043b9e32fdde9f36ae48
Wes Thorburn <wjthorburn@live.com.au>
2016-05-31 20:26:10 +1000

    Add text overlay on homepage image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <div class="row">
         <div class="col-md-12">
         	<img class="img-responsive" src="images/homepage-image-main.jpg">
+        	<h1 class="main-overlay-title">A programmer's resource for storing and managing notes and processes.</h1>
         </div>
     </div>
 </div>
