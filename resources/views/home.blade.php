a1b4b029a7e51a030cca9e968b4b56ed9146428a
Wes Thorburn <wjthorburn@live.com.au>
2016-05-31 20:12:37 +1000

    Add homepage responsive image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
+<div class="container margin-top-minus-20px">
     <div class="row">
         <div class="col-md-12">
-        	@if(\Auth::user())
-        		@include('layouts.partials.notes-page')
-	        @else
-	        	<div class="panel panel-default">
-	                <div class="panel-heading">Welcome</div>
-	                <div class="panel-body">
-	                    Your Application's home Page.
-	                </div>
-	            </div>
-	        @endif
-            
+        	<img class="img-responsive" src="images/homepage-image-main.jpg">
         </div>
     </div>
 </div>
