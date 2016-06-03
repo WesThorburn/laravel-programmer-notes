53bbfc5c2d37de7a13674b18b40393b6353335fa
Wes Thorburn <wjthorburn@live.com.au>
2016-06-03 20:56:36 +1000

    Add sign-up module to homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,15 @@
     <div class="row">
         <div class="col-md-12">
         	<img class="img-responsive" src="images/homepage-image-main.jpg">
-        	<h1 class="main-overlay-title">A programmer's resource for storing and managing notes and processes.</h1>
+        	<h1 class="main-overlay-title text-center">A programmer's resource for storing and managing notes and processes.</h1>
+        </div>
+        <div class="col-md-8 col-md-offset-2 margin-top-20px">
+        	<h2 class="sign-up-title text-center">Sign up to start creating your own notes! (It's free)</h2>
+        </div>
+        <div class="col-md-3 col-md-offset-5">
+        	<div class="center-block">
+	        	<button type="button" class="btn btn-primary btn-lg sign-up-button">Sign Up</button>
+	        </div>
         </div>
     </div>
 </div>
